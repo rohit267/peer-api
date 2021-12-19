8ee2e24342139bcdaf794a42531c77e12add6c2a
Rohit Mahto <rohitmahto267@gmaill.com>
2021-12-19 18:06:14 +0530

    changes

---
+++ app.js
@@ -0,0 +1,72 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var indexRouter = require("./routes/index");
+var http = require("http");
+const SocketService = require("./services/SocketService");
+
+var app = express();
+
+var port = process.env.PORT || "3000";
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+var server = http.createServer(app);
+
+SocketService.listen(server);
+app.set("socket", SocketService);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Socket server working on" + bind);
+}
+
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+// module.exports = app;
